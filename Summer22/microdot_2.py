089d2305c97bc0dce0ee997723d4e14902bb9a1d
William E Hahn <williamedwardhahn@gmail.com>
2022-07-13 18:22:54 -0400

    Create microdot_2.py

---
+++ Summer22/microdot_2.py
@@ -0,0 +1,43 @@
+from microdot import Microdot, send_file
+app = Microdot()
+
+
+html1 ="""
+<html>
+    <body >
+            <h1>Microdot Form Example</h1>
+
+            <form method="POST" action="">
+                
+                    <p>
+                        <input type="radio" name="var1" value="1">
+                        <input type="radio" name="var1" value="2">
+                        <input type="radio" name="var1" value="3">
+                        <br>
+                        <input type="submit" name="read" value="Read">
+                    </p>
+                
+                
+            </form>
+    </body>
+</html>
+"""
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index(request):
+
+	if request.method == 'POST':
+		print(request.form['var1'])
+
+	return html1, 200, {'Content-Type': 'text/html'}
+
+
+@app.route('/hi')
+def hi(request):
+	print("Run python here")
+	return "Hello", 200, {'Content-Type': 'text/html'}
+
+
+app.run(debug=True)
